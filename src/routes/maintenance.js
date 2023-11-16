8366200a8dd89154589147b6b792bd1eb9d52266
sunghyun.an <sunghyun.an@gmail.com>
2023-11-16 16:52:10 +0900

    관리자 유지보수 접근 수정

--- src/routes/maintenance.js
+++ src/routes/maintenance.js
@@ -3,22 +3,34 @@ const router = express.Router();
 
 const maintenanceController = require('../controllers/maintenance');
 const multerMiddleware = require('../middleware/multer');
+const isAuthMiddleware = require('../middleware/is-auth');
 
-router.get('/list/:category', maintenanceController.getMaintenanceBoardList); // 유지보수 게시판 리스트
+router.get(
+   '/list/:category',
+   isAuthMiddleware.isAuthAdmin,
+   maintenanceController.getMaintenanceBoardList
+); // 유지보수 게시판 리스트
 router.get(
    '/view/:category/:list_no',
+   isAuthMiddleware.isAuthAdmin,
    maintenanceController.getMaintenanceBoardView
 ); // 게시판 조회
 
 router.post(
    '/create',
+   isAuthMiddleware.isAuthAdmin,
    multerMiddleware.fileMulter,
    maintenanceController.getMaintenanceBoardCreate
 ); // 게시판 등록
 
-router.post('/comment', maintenanceController.postMaintenanceCommentCreate); // 게시판 댓글 등록
+router.post(
+   '/comment',
+   isAuthMiddleware.isAuthAdmin,
+   maintenanceController.postMaintenanceCommentCreate
+); // 게시판 댓글 등록
 router.get(
    '/comment/:list_no',
+   isAuthMiddleware.isAuthAdmin,
    maintenanceController.getMaintenanceCommentList
 ); // 게시판 댓글 리스트
 
