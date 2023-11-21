91bb2295b308705615ef323ce1bf8bd8632d0285
sunghyun.an <sunghyun.an@gmail.com>
2023-11-21 12:20:38 +0900

    오리지널 이름 변경

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -13,7 +13,9 @@ const fileStorage = (destination) =>
          //    'utf8'
          // ); 1.4.4 로 변경
 
-         const originalName = file.originalname;
+         const originalName = Buffer.from(file.originalname, 'latin1').toString(
+            'utf8'
+         );
          const _fileLen = originalName.length;
          const _lastDot = originalName.lastIndexOf('.') + 1;
          const _fileNameWithoutExt = originalName.substring(0, _lastDot - 1);
