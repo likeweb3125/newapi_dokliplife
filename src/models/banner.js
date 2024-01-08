9d412213ab679ebfe1531dea7c18cb05e54d2769
sunghyun.an <sunghyun.an@gmail.com>
2024-01-08 15:30:41 +0900

    동영상 URL 이름 수정

--- src/models/banner.js
+++ src/models/banner.js
@@ -48,7 +48,7 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.STRING(50),
             allowNull: true,
          },
-         b_move_url: {
+         b_mov_url: {
             type: DataTypes.STRING(500),
             allowNull: true,
          },
